9b2aba871c768232c185c41bd6e7b2459dad1774
BATUHAN UZUNOGLU <batuhan.uzunoglu@ext.digiturk.com.tr>
2022-10-23 23:41:38 +0300

    Add firebase options for web

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:panda_flutter/responsive/mobile_screen_layout.dart';
 import 'package:panda_flutter/responsive/responsive_layout_screen.dart';
@@ -7,7 +8,20 @@ import 'package:panda_flutter/utils/colors.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyCHeJWLeoeHzQ0zD_Ip-QC0s6rmiHGZneU',
+        appId: '1:1034439349249:web:d30de96987b4a8dbd35c00',
+        messagingSenderId: '1034439349249',
+        projectId: 'panda-149d9',
+        storageBucket: 'panda-149d9.appspot.com',
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
