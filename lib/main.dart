4bafca081ced8d8ce5b05d4de6126a7d534a4da0
BATUHAN UZUNOGLU <batuhan.uzunoglu@ext.digiturk.com.tr>
2022-10-23 23:10:17 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:panda_flutter/responsive/mobile_screen_layout.dart';
+import 'package:panda_flutter/responsive/responsive_layout_screen.dart';
+import 'package:panda_flutter/responsive/web_screen_layout.dart';
+import 'package:panda_flutter/utils/colors.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Panda',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: mobileBackgroundColor,
+      ),
+      home: const ResponsiveLayout(
+        mobileScreenLayout: MobileScreenLayout(),
+        webScreenLayout: WebScreenLayout(),
+      ),
+    );
+  }
+}
